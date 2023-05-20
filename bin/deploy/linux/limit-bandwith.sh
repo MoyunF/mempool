5aa48c81329c0e36f19b6afd2f030348fb802cf0
lxx <251530526@qq.com>
2023-05-20 15:27:19 +0800

    执行没有修改，不限制微块广播量

--- bin/deploy/linux/limit-bandwith.sh
+++ bin/deploy/linux/limit-bandwith.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-# 获取所有以"veth"开头的网卡名称
-interfaces=$(ifconfig -a | grep -oP 'veth[a-z0-9]+' | sort)
+# # 获取所有以"veth"开头的网卡名称
+# interfaces=$(ifconfig -a | grep -oP 'veth[a-z0-9]+' | sort)
 
-# 设置带宽限制的值（以Kbps为单位）
-bandwidth_limit="10240"
+# # 设置带宽限制的值（以Kbps为单位）
+# bandwidth_limit="10240"
 
-# 计数器初始化为0
+# # 计数器初始化为0
+# count=0
+
+# # 遍历每个网卡并限制带宽
+# for interface in $interfaces; do
+#   echo "清楚网卡 $interface 带宽限制"
+#   sudo wondershaper $interface $1 $1 
+#   count=$((count+1))
+# done
+
+# echo "共限制了 $count 个网卡的带宽。"
+#!/bin/bash
+
+#!/bin/bash
+
+interfaces=($(ifconfig -a | grep -oP 'veth[a-z0-9]+' | sort))
 count=0
+half=$((${#interfaces[@]}/2))
 
-# 遍历每个网卡并限制带宽
-for interface in $interfaces; do
-  echo "清楚网卡 $interface 带宽限制"
-  sudo wondershaper $interface $1 $1 
+for ((i=0; i<${#interfaces[@]}; i++)); do
+  interface=${interfaces[$i]}
+  if [ $i -lt $half ]; then
+    echo "正在限制 $interface 网卡的带宽为 $1 Kbps"
+    sudo wondershaper $interface $1 $1 
+  else
+    echo "正在限制 $interface 网卡的带宽为 $2 Kbps"
+    sudo wondershaper $interface $2 $2 
+  fi
   count=$((count+1))
 done
 
-echo "共限制了 $count 个网卡的带宽。"
+echo "已为 $count 个网卡限制了带宽。"
